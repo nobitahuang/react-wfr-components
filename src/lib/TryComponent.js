63627c16d465481010b75f8de73441077c4ba39d
Nobita <nobita.h@weefer.co.id>
2019-11-03 04:15:37 +0700

    Initialize Commit

---
+++ src/lib/TryComponent.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+export default class TryComponent extends React.Component {
+    render() {
+        return <div>this is Try Component</div>;
+    }
+}
---
+++ src/lib/index.js
@@ -0,0 +1,9 @@
+import DummyComponent from './DummyComponent';
+import TryComponent from './TryComponent';
+import Button from './Button';
+
+export {
+    DummyComponent,
+    TryComponent,
+    Button
+}
